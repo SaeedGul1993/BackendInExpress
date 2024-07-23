dd2ac322e0f4fca2daeaf96055d8a37a686cc6b4
SaeedGul1993 <42095477+SaeedGul1993@users.noreply.github.com>
2024-07-24 01:15:01 +0500

    env file added

--- src/utils/sendGrid/sendGrid.config.mjs
+++ src/utils/sendGrid/sendGrid.config.mjs
@@ -1,8 +1,7 @@
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
 
-const sendGridApiKey =
-  "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";
+const sendGridApiKey = process.env.sendGridAPIKey;
 
 const createSendGridConnection = () => {
   let options = {
